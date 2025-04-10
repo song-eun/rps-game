4bf31dfc371f881ee133a7b61b6860fd40a384fa
song_eun <dmswo546@gmail.com>
2025-04-10 17:12:41 +0900

    style: 버튼 스타일 추가

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,14 +1,30 @@
+import paperImg from "../assets/paper.png";
+import rockImg from "../assets/rock.png";
+import scissorsImg from "../assets/scissors.png";
 import css from "../css/Button.module.css";
+
+const imageMap = {
+  보: paperImg,
+  가위: scissorsImg,
+  바위: rockImg,
+};
 
 const Button = ({ choice, handleUserChoice }) => {
   const handleBtn = () => {
     handleUserChoice(choice);
   };
 
+  const labelMap = {
+    바위: "rock",
+    보: "paper",
+    가위: "scissors",
+  };
+
   return (
-    <div className={css.btnContainer}>
-      <button onClick={handleBtn}>{choice}</button>
-    </div>
+    <button className={css[labelMap[choice]]} onClick={handleBtn}>
+      <img src={imageMap[choice]} alt={choice} />
+      {choice}
+    </button>
   );
 };
 
